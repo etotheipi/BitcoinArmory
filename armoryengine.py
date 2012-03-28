fa7221d113adec5afed6597c0a18e1d512c8fe3d
etotheipi <etotheipi@gmail.com>
2012-03-28 17:34:26 -0400

    Updated all copyright statements to incl 2012

--- armoryengine.py
+++ armoryengine.py
@@ -1,6 +1,6 @@
 ################################################################################
 #
-# Copyright (C) 2011, Alan C. Reiner    <alan.reiner@gmail.com>
+# Copyright (C) 2011-2012, Alan C. Reiner    <alan.reiner@gmail.com>
 # Distributed under the GNU Affero General Public License (AGPL v3)
 # See LICENSE or http://www.gnu.org/licenses/agpl.html
 #
--- cppForSwig/EncryptionUtils.cpp
+++ cppForSwig/EncryptionUtils.cpp
@@ -1,3 +1,10 @@
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+//  Copyright (C) 2011-2012, Alan C. Reiner    <alan.reiner@gmail.com>        //
+//  Distributed under the GNU Affero General Public License (AGPL v3)         //
+//  See LICENSE or http://www.gnu.org/licenses/agpl.html                      //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
 #include "EncryptionUtils.h"
 #include "integer.h"
 #include "oids.h"
