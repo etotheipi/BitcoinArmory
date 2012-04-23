182c4e49400f23bfbf0372f766640d7d098140f1
Alan Reiner <etotheipi@gmail.com>
2012-04-22 21:58:19 -0700

    Modifying HKEY_CURRENT_USER in registry successfully!
    ... but the URI is not getting handled...

--- windowsinstall/register_uri.py
+++ windowsinstall/register_uri.py
@@ -8,15 +8,43 @@ x86str = ''
 if osBits=='64':
    x86str = ' (x86)'
 
-updateKeys = []
-updateKeys.append([r'bitcoin', '', None])
-updateKeys.append([r'bitcoin', 'URL Protocol', ""])
-updateKeys.append([r'bitcoin\shell', '', None])
-updateKeys.append([r'bitcoin\shell\open', '',  None])
-updateKeys.append([r'bitcoin\shell\open\command',  '', \
-   r'"C:\Program Files%s\Armory\Armory Bitcoin Client\Armory.exe" %1'])
-updateKeys.append([r'bitcoin\DefaultIcon', '',   \
-   r'"C:\Program Files%s\Armory\Armory Bitcoin Client\armory48x48.ico'])
+baseDir = 'C:\\Program Files%s\\Armory\\Armory Bitcoin Client' % x86str
 
-for k,v in updateKeys:
-   key = CreateKey(HKEY_CURRENT_USER, 
+updateKeys = []
+updateKeys.append(['bitcoin', '', 'URL:bitcoin Protocol'])
+updateKeys.append(['bitcoin', 'URL Protocol', ""])
+updateKeys.append(['bitcoin\\shell', '', None])
+updateKeys.append(['bitcoin\\shell\\open', '',  None])
+updateKeys.append(['bitcoin\\shell\\open\\command',  '', \
+                   '"%s\\Armory.exe" %%1' % baseDir])
+updateKeys.append(['bitcoin\\DefaultIcon', '',  \
+                   '"%s\\armory48x48.ico"' % baseDir])
+
+print 'Attempting to read registry keys...'
+
+# First get the existing keys
+origRegistry = {}
+for key,name,val in updateKeys:
+   dkey = '%s\\%s' % (key,name)
+   print '\tReading key: [HKEY_CURRENT_USER]\\]' + dkey
+   try:
+      registryKey = OpenKey(HKEY_CURRENT_USER, key, 0, KEY_READ)
+      origRegistry[dkey] = QueryValueEx(registryKey, name)
+   except:
+      raise
+      origRegistry[dkey] = ''
+
+print 'Keys read:'
+for k,v in origRegistry.iteritems():
+   print k,v[0]
+   print '\t', k.ljust(40), '"%s"'%v[0]
+
+print 'Attempting to write registry keys...'
+
+for key,name,val in updateKeys:
+   dkey = '%s\\%s' % (key,name)
+   print '\tWriting key: [HKEY_CURRENT_USER\\]' + dkey
+   registryKey = CreateKey(HKEY_CURRENT_USER, key)
+   SetValueEx(registryKey, name, 0, REG_SZ, val)
+   CloseKey(registryKey)
+      
