0863ca0d194f611251221c0a0e6c476146cc32c8
AndyOfiesh <andy@bitcoinarmory.com>
2013-10-31 12:06:13 -0400

    ARMORY-33 - Add Icon files to NSIS installer

--- build_installer.bat
+++ build_installer.bat
@@ -2,6 +2,8 @@ REM This should only be run from cppForSwig\BitcoinArmory_SwigDLL directory
 copy ..\libs\Win32\BitcoinArmory_SwigDLL.dll ..\..\_CppBlockUtils.pyd 
 C:\Python27\Lib\site-packages\PyQt4\pyrcc4.exe -o ..\..\qrc_img_resources.py ..\..\imgList.xml
 python ..\..\setup.py py2exe --includes sip,hashlib,json,twisted -d ..\..\ArmoryStandalone
+copy ..\..\img\*.ico ..\..\ArmoryStandalone
+copy ..\..\img\armory_logo*.png ..\..\ArmoryStandalone
 rtc /F:..\..\edit_icons.rts
 python ..\..\writeNSISCompilerArgs.py
 makensis.exe ..\..\ArmorySetup.nsi
