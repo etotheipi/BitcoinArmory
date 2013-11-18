96bef065ccc3becf3a029a8cfbf8fe6fd0fd5a8f
goatpig <moothecowlord@gmail.com>
2013-11-18 23:43:11 +0100

    changed py2exe setup to yield a single exe with all the dependencies bundled inside it (bitcoin-qt style)

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@ from distutils.core import setup
 import py2exe
 
 
-opts = {"py2exe": {"dll_excludes": ["MSWSOCK.dll", "IPHLPAPI.dll", "MSWSOCK.dll", "WINNSI.dll", "WTSAPI32.dll"]}}
+opts = {"py2exe":{
+    "dll_excludes":["MSWSOCK.dll", "IPHLPAPI.dll", "MSWSOCK.dll", "WINNSI.dll", "WTSAPI32.dll"],
+    "bundle_files": 1
+    }}
 
-setup( options = opts, windows = ['../../ArmoryQt.py'] )
+setup( options = opts, windows = ['../../ArmoryQt.py'], zipfile = None )
 
    #options = {'py2exe': {'bundle_files': 1}}, \
    #zipfile = None )
