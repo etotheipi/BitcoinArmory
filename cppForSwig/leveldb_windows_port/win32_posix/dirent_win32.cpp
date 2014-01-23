e8bde9aee41c049ef9ea2f612c8216459ad12167
goatpig <moothecowlord@gmail.com>
2014-01-23 01:07:43 -0500

    fixing mess up in /win32_posix

--- cppForSwig/leveldb_windows_port/win32_posix/dirent_win32.cpp
+++ cppForSwig/leveldb_windows_port/win32_posix/dirent_win32.cpp
@@ -14,30 +14,28 @@
 #include <io.h> /* _findfirst and _findnext set errno iff they return -1 */
 #include <stdlib.h>
 #include <string.h>
-
-#ifdef __cplusplus
-extern "C"
-{
-#endif
+#include <Windows.h>
 
 typedef ptrdiff_t handle_type; /* C99's intptr_t not sufficiently portable */
 
 struct DIR
 {
     handle_type         handle; /* -1 for failed rewind */
-    struct _finddata_t  info;
+    //struct _finddata_t  info;
+    struct _wfinddata_t winfo;
     struct dirent       result; /* d_name null iff first time */
-    char                *name;  /* null-terminated char string */
+    //char                *name;  /* null-terminated char string */
+    wchar_t             *wname;  /* null-terminated wchar string */
 };
 
-DIR *opendir(const char *name)
+/*DIR *opendir(const char *name)
 {
     DIR *dir = 0;
 
     if(name && name[0])
     {
         size_t base_length = strlen(name);
-        const char *all = /* search pattern must end with suitable wildcard */
+        const char *all = 
             strchr("/\\", name[base_length - 1]) ? "*" : "/*";
 
         if((dir = (DIR *) malloc(sizeof *dir)) != 0 &&
@@ -58,9 +56,73 @@ DIR *opendir(const char *name)
             {
                 dir->result.d_name = 0;
             }
-            else /* rollback */
+            else
             {
                 free(dir->name);
+                free(dir);
+                dir = 0;
+            }
+        }
+        else
+        {
+            free(dir);
+            dir   = 0;
+            errno = ENOMEM;
+        }
+    }
+    else
+    {
+        errno = EINVAL;
+    }
+
+    return dir;
+}*/
+
+DIR *opendir(const char *name)
+{
+	wchar_t *namew = (wchar_t*)malloc(sizeof(wchar_t)*(strlen(name)+1));
+	MultiByteToWideChar(CP_UTF8, 0, name, -1, namew, strlen(name)+1);
+	
+	DIR *rtdir = opendir(namew);
+	free(namew);
+	
+	return rtdir;
+}
+
+DIR *opendir(const wchar_t *name)
+{
+    DIR *dir = 0;
+
+    if(name && name[0])
+    {
+        size_t base_length = wcslen(name);
+        const wchar_t *all = /* search pattern must end with suitable wildcard */
+            wcschr(L"/\\", name[base_length - 1]) ? L"*" : L"/*";
+
+        if((dir = (DIR *) malloc(sizeof *dir)) != 0 &&
+           (dir->wname = (wchar_t *) malloc(sizeof(wchar_t)*(base_length + wcslen(all) + 2))) != 0)
+        {
+			dir->result.d_name = 0;
+
+			//make sure '.' is prepended to paths starting with '/'
+			if(name[0]==L'/') 
+			{
+				dir->wname[0] = L'.';
+				dir->wname[1] = 0;
+			}
+			else dir->wname[0] = 0;
+            
+			wcscat(wcscat(dir->wname, name), all);
+
+            if((dir->handle =
+                (handle_type) _wfindfirst(dir->wname, &dir->winfo)) != -1)
+            {
+                dir->result.wd_name = 0;
+				dir->result.d_name = (char*)malloc(MAX_PATH);
+            }
+            else /* rollback */
+            {
+                free(dir->wname);
                 free(dir);
                 dir = 0;
             }
@@ -91,8 +153,9 @@ int closedir(DIR *dir)
             result = _findclose(dir->handle);
         }
 
-        free(dir->name);
-        free(dir);
+        if(dir->result.d_name) free(dir->result.d_name);
+        free(dir->wname);
+       free(dir);
     }
 
     if(result == -1) /* map all errors to EBADF */
@@ -109,10 +172,11 @@ struct dirent *readdir(DIR *dir)
 
     if(dir && dir->handle != -1)
     {
-        if(!dir->result.d_name || _findnext(dir->handle, &dir->info) != -1)
+        if(!dir->result.wd_name || _wfindnext(dir->handle, &dir->winfo) != -1)
         {
             result         = &dir->result;
-            result->d_name = dir->info.name;
+            result->wd_name = dir->winfo.name;
+			WideCharToMultiByte(CP_UTF8, 0, dir->winfo.name, -1, result->d_name, MAX_PATH, 0, 0);
         }
     }
     else
@@ -128,18 +192,15 @@ void rewinddir(DIR *dir)
     if(dir && dir->handle != -1)
     {
         _findclose(dir->handle);
-        dir->handle = (handle_type) _findfirst(dir->name, &dir->info);
-        dir->result.d_name = 0;
+        dir->handle = (handle_type) _wfindfirst(dir->wname, &dir->winfo);
+        dir->result.wd_name = 0;
+		dir->result.d_name[0] = 0;
     }
     else
     {
         errno = EBADF;
     }
 }
-
-#ifdef __cplusplus
-}
-#endif
 
 /*
 
