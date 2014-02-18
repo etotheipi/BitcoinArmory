3bd44b0ee1941bf521ac86ea0ce41d9ae02a75a5
Doug <doug@bitcoinarmory.com>
2014-02-18 17:57:19 -0500

    DEVDISC-19 OSX Command-line Arguments
    Reverts the 10.7 restriction and adds a warning for users.

--- osxbuild/Armory-script.sh
+++ osxbuild/Armory-script.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # This is the initial driver script executed by the Armory application on OS X.
 # Its role is to set up the environment before passing control to Python.
+# WARNING: For unknown reasons, OS X seems to be very touchy about whether or
+# not the program will run if you double-click it, or use "open -a" on it. You
+# may have to manually invoke this script in order to run Armory.
 
 # Set environment variables so the Python executable finds its stuff.
-# Note that `dirname $0` doesn't always work. What's here is more robust.
+# Note that `dirname $0` doesn't always work. This gives the absolute path.
 DIRNAME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ARMORYDIR="$DIRNAME/py/lib/armory"
 LIBDIR="$DIRNAME/../Dependencies"
@@ -14,14 +17,9 @@ export DYLD_LIBRARY_PATH="${LIBDIR}:${FRDIR}"
 export DYLD_FRAMEWORK_PATH="${LIBDIR}:${FRDIR}"
 
 # OS X chokes if you try to pass command line args when none exist. Pass only
-# if there are args to pass. 10.7 (Lion) also has a quirk where $#=1 if there
-# are no args. Adjust for it in order for Armory to run. Unfortunately, there
-# seems to be no good way to pass args in 10.7, as $#=1 when there's 1 arg!
-# Users can work around this by passing multiple args.
+# if there are args to pass.
 OSXVER=`sw_vers -productVersion | awk '{ print substr( $0, 0, 4 ) }'`
 if [ $# == "0" ]; then
-	$DIRNAME/Python $ARMORYDIR/ArmoryQt.py
-elif [ $# == "1" -a $OSXVER == "10.7" ]; then
 	$DIRNAME/Python $ARMORYDIR/ArmoryQt.py
 else
 	$DIRNAME/Python $ARMORYDIR/ArmoryQt.py "$@"
