f5f662f64fd039a020d6ebb19f87f53051981974
Alan Reiner <alan@bitcoinarmory.com>
2014-04-06 00:43:27 -0400

    0.91-beta!  Fixed version notify, and release scripts

--- release_scripts/Step1_Online_PrepareForSigning.py
+++ release_scripts/Step1_Online_PrepareForSigning.py
@@ -1,1 +1,25 @@
 # stub
+import subprocess
+import os
+import time
+import sys
+import shutil
+import ast
+import fetchInstallers
+from release_utils import execAndWait
+
+CLONE_URL = 'https://github.com/etotheipi/BitcoinArmory.git'
+
+verStr   = sys.argv[1]
+typeStr  = sys.argv[2]
+localDir = sys.argv[3]
+
+
+fetchInstallers.doFetch()
+cloneDir = os.path.join(localDir, 'BitcoinArmory')
+rscrDir  = os.path.join(localDir, 'release_scripts')
+
+execAndWait(['git', 'clone', CLONE_URL, cloneDir])
+shutil.copytree('../release_scripts', rscrDir)
+
+
