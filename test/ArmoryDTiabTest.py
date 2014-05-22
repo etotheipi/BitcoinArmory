913ff146f8a003d2012e10cd0d13b95702355817
AndyOfiesh <andy@bitcoinarmory.com>
2014-05-21 21:13:39 -0400

    Fixed import

--- test/ArmoryDTiabTest.py
+++ test/ArmoryDTiabTest.py
@@ -7,7 +7,7 @@ import os
 import unittest
 from test.Tiab import TiabTest
 from armoryengine.ArmoryUtils import *
-from armoryd import AmountToJSON, Armory_Json_Rpc_Server, JSONtoAmount, startArmoryDaemon
+from armoryd import AmountToJSON, Armory_Json_Rpc_Server, JSONtoAmount
 from armoryengine.BDM import TheBDM
 from armoryengine.PyBtcWallet import PyBtcWallet
 from armoryengine.Transaction import UnsignedTransaction
