faae0c8b1118e7d83f75db16f096726b3a51d64e
Alan Reiner <alan@bitcoinarmory.com>
2014-06-01 22:44:24 -0400

    Last commit was missing some dynamicImport code

--- dynamicImport.py
+++ dynamicImport.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 
 def getModuleList(inDir):
@@ -19,4 +20,46 @@ def getModuleList(inDir):
       if not modName in moduleMap:
          moduleMap[modName] = {}
       
+      if fn.endswith('.py'):
+         moduleMap[modName]['Source'] = fileData
+      elif fn.endswith('.sig'):
+         moduleMap[modName]['Signature'] = fileData
       
+      
+
+def dynamicImport(inDir, moduleName, injectLocals=None):
+   """
+   We import from an arbitrary directory, we need to add the dir
+   to sys.path, but we want to prevent any shenanigans by an imported
+   module that messes with sys.path (perhaps maliciously, but trying
+   to import a malicious module without allowing malicious behavior
+   is probably impossible--make sure you're using safe code and then
+   assume the module is safe). 
+
+   Either way, we're going to assume that the dynamically-imported
+   modules are really simple and have no reason to mess with sys.path.
+   We will revisit this later if it becomes a barrier to being useful
+   """
+   if injectLocals is None:
+      injectLocals = {}
+
+   if not os.path.exists(inDir, moduleName+'.py'):
+      return None
+
+   # Join using a character that would be invalid in a pathname
+   prevSysPath = '\x00'.join(sys.path)
+   sys.path.append(inDir)
+
+   modTemp = __import__(moduleName)
+   modTemp.__dict__.update(injectLocals)
+
+   # Assume that sys.path was unmodified by the module
+   sys.path = sys.path[:-1]
+   currSysPath = '\x00'.join(sys.path)
+   if not currSysPath==prevSysPath:
+      print '***ERROR: Dynamically imported module messed with sys.path!'
+      print '        : Make sure your module does not modify sys.path'
+      exit(1)
+   
+   return modTemp
+
