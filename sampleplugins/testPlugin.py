754035286f17ae9bf641acfd5e60263dd0c8bbc7
Alan Reiner <alan@bitcoinarmory.com>
2014-06-03 01:46:11 -0400

    Added balance conversion table, though not debugged yet

--- sampleplugins/testPlugin.py
+++ sampleplugins/testPlugin.py
@@ -18,41 +18,47 @@ class PluginObject(object):
    tabName = 'Exchange Rates'
    
    #############################################################################
-   def __init__(self, parent):
+   def __init__(self, main):
 
-      self.parent = parent
+      self.main = main
 
+      ##########################################################################
+      ##### Display the conversion values based on the Coinbase API
       self.lastPriceFetch = 0
-      self.lblHeader    = QRichLabel(tr("""Tracking buy and sell prices on 
-         Coinbase every 60 seconds"""), doWrap=False)
-      self.lblLastTime  = QRichLabel('')
+      self.lblHeader    = QRichLabel(tr("""<b>Tracking buy and sell prices on 
+         Coinbase every 60 seconds</b>"""), doWrap=False)
+      self.lblLastTime  = QRichLabel('', doWrap=False)
       self.lblSellLabel = QRichLabel(tr('Coinbase <b>Sell</b> Price (USD):'), doWrap=False)
       self.lblBuyLabel  = QRichLabel(tr('Coinbase <b>Buy</b> Price (USD):'),  doWrap=False)
       self.lblSellPrice = QRichLabel('<Not Available>')
       self.lblBuyPrice  = QRichLabel('<Not Available>')
 
       self.btnUpdate = QPushButton(tr('Check Now'))
-      parent.connect(self.btnUpdate, SIGNAL('clicked()'), self.checkUpdatePrice)
+      self.main.connect(self.btnUpdate, SIGNAL('clicked()'), self.checkUpdatePrice)
 
+      ##########################################################################
+      ##### A calculator for converting prices between USD and BTC
       lblCalcTitle = QRichLabel(tr("""Convert between USD and BTC using 
          Coinbase sell price"""), hAlign=Qt.AlignHCenter, doWrap=False)
       self.edtEnterUSD = QLineEdit()
       self.edtEnterBTC = QLineEdit()
-      self.lblEnterUSD = QRichLabel('USD ($)')
+      self.lblEnterUSD1 = QRichLabel('$')
+      self.lblEnterUSD2 = QRichLabel('USD')
       self.lblEnterBTC = QRichLabel('BTC')
       btnClear = QPushButton('Clear')
 
-      parent.connect(self.edtEnterUSD, SIGNAL('textEdited(QString)'), self.updateCalcBTC)
-      parent.connect(self.edtEnterBTC, SIGNAL('textEdited(QString)'), self.updateCalcUSD)
+      self.main.connect(self.edtEnterUSD, SIGNAL('textEdited(QString)'), self.updateCalcBTC)
+      self.main.connect(self.edtEnterBTC, SIGNAL('textEdited(QString)'), self.updateCalcUSD)
 
       def clearCalc():
          self.edtEnterUSD.setText('')
          self.edtEnterBTC.setText('')
 
-      parent.connect(btnClear, SIGNAL('clicked()'), clearCalc)
+      self.main.connect(btnClear, SIGNAL('clicked()'), clearCalc)
 
-      frmCalcMid = makeHorizFrame( [self.edtEnterUSD,
-                                    self.lblEnterUSD,
+      frmCalcMid = makeHorizFrame( [self.lblEnterUSD1,
+                                    self.edtEnterUSD,
+                                    self.lblEnterUSD2,
                                     'Stretch',
                                     self.edtEnterBTC,
                                     self.lblEnterBTC])
@@ -60,21 +66,44 @@ class PluginObject(object):
       frmCalcClear = makeHorizFrame(['Stretch', btnClear, 'Stretch'])
       frmCalc = makeVertFrame([lblCalcTitle, frmCalcMid, frmCalcClear], STYLE_PLAIN)
 
+      ##########################################################################
+      ##### A table showing you the total balance of each wallet in USD and BTC
+      lblWltTableTitle = QRichLabel(tr("Wallet balances converted to USD"), 
+                                            doWrap=False, hAlign=Qt.AlignHCenter)
+      numWallets = len(self.main.walletMap)
+      self.wltTable = QTableWidget(self.main)
+      self.wltTable.setRowCount(numWallets)
+      self.wltTable.setColumnCount(4)
+      self.wltTable.horizontalHeader().setStretchLastSection(True)
+      self.wltTable.setMinimumWidth(500)
+
+
+      ##########################################################################
+      ##### Setup the main layout for the tab
       mainLayout = QGridLayout()
-      mainLayout.addWidget(self.lblHeader,      0,0,  1,3)
-      mainLayout.addItem(QSpacerItem(15,15),    1,0)
-      mainLayout.addWidget(self.lblSellLabel,   1,1)
-      mainLayout.addWidget(self.lblSellPrice,   1,2)
-      mainLayout.addItem(QSpacerItem(15,15),    2,0)
-      mainLayout.addWidget(self.lblBuyLabel,    2,1)
-      mainLayout.addWidget(self.lblBuyPrice,    2,2)
-      mainLayout.addWidget(self.lblLastTime,    3,0,  1,3)
-      mainLayout.addWidget(self.btnUpdate,      4,2)
-
-      mainLayout.addItem(QSpacerItem(20,20),    5,1)
-
-      mainLayout.addWidget(frmCalc,             6,0,  1,3)
-      
+      i=0
+      mainLayout.addWidget(self.lblHeader,      i,0,  1,3)
+      i+=1
+      mainLayout.addItem(QSpacerItem(15,15),    i,0)
+      mainLayout.addWidget(self.lblSellLabel,   i,1)
+      mainLayout.addWidget(self.lblSellPrice,   i,2)
+      i+=1
+      mainLayout.addItem(QSpacerItem(15,15),    i,0)
+      mainLayout.addWidget(self.lblBuyLabel,    i,1)
+      mainLayout.addWidget(self.lblBuyPrice,    i,2)
+      i+=1
+      mainLayout.addWidget(self.lblLastTime,    i,0,  1,2)
+      mainLayout.addWidget(self.btnUpdate,      i,2)
+      i+=1
+      mainLayout.addItem(QSpacerItem(20,20),    i,0)
+      i+=1
+      mainLayout.addWidget(frmCalc,             i,0,  1,3)
+      i+=1
+      mainLayout.addItem(QSpacerItem(30,30),    i,0)
+      i+=1
+      mainLayout.addWidget(lblWltTableTitle,    i,0,  1,3)
+      i+=1
+      mainLayout.addWidget(self.wltTable,       i,0,  1,3)
 
       mainLayout.setColumnStretch(0,0)
       mainLayout.setColumnStretch(1,1)
@@ -136,6 +165,7 @@ class PluginObject(object):
          LOGEXCEPT('Failed to fetch price data from %s' % urlBase)
 
       self.updateLastTimeStr()
+      self.updateWalletTable()
    
 
    #############################################################################
@@ -180,6 +210,39 @@ class PluginObject(object):
          LOGEXCEPT('ERR')  # delete me... just for debugging
          self.edtEnterUSD.setText('')
       
+      
+   #############################################################################
+   def updateWalletTable(self):
+      numWallets = len(self.main.walletMap)
+      self.wltTable.setRowCount(numWallets)
+      self.wltTable.setColumnCount(4)
+
+      row = 0
+      for wltID,wltObj in self.main.walletMap.iteritems():
+         wltValueBTC = '(...)'
+         wltValueUSD = '(...)'
+         if TheBDM.getBDMState()=='BlockchainReady':
+            convertVal = float(self.lastSellStr.replace(',',''))
+            wltBal = wlt.getBalance('Total')
+            wltValueBTC = coin2str(wltBal, maxZeros=2)
+            wltValueUSD = self.addCommasToPrice('$%0.2f' % (wltBal*convertVal))
+
+         rowItems = []
+         rowItems.append(QTableWidgetItem(wltID))
+         rowItems.append(QTableWidgetItem(wltObj.labelName))
+         rowItems.append(QTableWidgetItem(wltValueBTC))
+         rowItems.append(QTableWidgetItem(wltValueUSD))
+
+         for i,item in enumerate(rowItems):
+            self.wltTable.setItem(row, i, item)
+
+         self.wltTable.setHorizontalHeaderItem(0, QTableWidgetItem(tr('Wallet ID')))
+         self.wltTable.setHorizontalHeaderItem(1, QTableWidgetItem(tr('Wallet Name')))
+         self.wltTable.setHorizontalHeaderItem(2, QTableWidgetItem(tr('BTC Balance')))
+         self.wltTable.setHorizontalHeaderItem(3, QTableWidgetItem(tr('USD ($) Value')))
+         self.wltTable.verticalHeader().hide()
+
+         row += 1
       
 
 
