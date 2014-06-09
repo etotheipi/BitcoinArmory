0ad8bfe8a6c53bd916692b8a3464ac78b4bb231c
AndyOfiesh <andy@bitcoinarmory.com>
2014-06-09 13:25:58 -0400

    End of the test sub-module experiment Putting all tests back into the main repo, and add tiab.zip

---
+++ test/TransactionTest.py
@@ -0,0 +1,31 @@
+'''
+Created on Jan 10, 2014
+
+@author: Alan
+'''
+from test.Tiab import TiabTest
+import sys
+import unittest
+
+sys.path.append('..')
+from armoryengine.ALL import *
+
+
+# Unserialize an reserialize
+
+class TransactionTest(TiabTest):
+
+   
+   def setUp(self):
+      pass
+   
+   
+   def tearDown(self):
+      pass
+   
+
+   
+   
+if __name__ == "__main__":
+   #import sys;sys.argv = ['', 'Test.testName']
+   unittest.main()
