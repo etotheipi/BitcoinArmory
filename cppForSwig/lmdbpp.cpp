0d1d5ca39b49a4decfd8d898fbd8f8ce8c5bd817
Charles Samuels <charles@bitcoinarmory.com>
2014-06-30 15:20:25 -0700

    missing files

---
+++ cppForSwig/lmdbpp.cpp
@@ -0,0 +1,356 @@
+#include "lmdbpp.h"    
+
+#include <lmdb.h>
+#include <map>
+#include <unistd.h>
+
+#include <sstream>
+#include <cstring>
+
+//#define DISABLE_TRANSACTIONS
+
+static std::string errorString(int rc)
+{
+   return mdb_strerror(rc);
+}
+
+void LMDB::Iterator::reset()
+{
+   if (csr_)
+      mdb_cursor_close(csr_);
+}
+
+inline void LMDB::Iterator::checkHasDb() const
+{
+   if (!db_)
+   {
+      throw std::logic_error("Iterator is not associated with a db");
+   }
+}
+
+
+inline void LMDB::Iterator::checkOk() const
+{
+   if (!isValid())
+   {
+      throw std::logic_error("Tried to use invalid LMDB Iterator");
+   }
+}
+
+LMDB::Iterator::Iterator(const LMDB *db)
+   : db_(db), csr_(nullptr), has_(false)
+{
+   if (!db->txn)
+      throw std::runtime_error("Iterator must be created within Transaction");
+   
+   int rc;
+   rc = mdb_cursor_open(db->txn, db->dbi, &csr_);
+   if (rc != MDB_SUCCESS)
+   {
+      LMDBException e("Failed to open cursor (" + errorString(rc) + ")");
+      throw e;
+   }
+}
+
+LMDB::Iterator::~Iterator()
+{
+   reset();
+}
+
+LMDB::Iterator::Iterator(const Iterator &copy)
+{
+   operator=(copy);
+}
+
+LMDB::Iterator::Iterator(Iterator &&move)
+{
+   operator=(std::move(move));
+}
+
+LMDB::Iterator& LMDB::Iterator::operator=(const Iterator &copy)
+{
+   throw std::logic_error("not implemented");
+}
+
+LMDB::Iterator& LMDB::Iterator::operator=(Iterator &&move)
+{
+   std::swap(db_, move.db_);
+   std::swap(csr_, move.csr_);
+   std::swap(has_, move.has_);
+   std::swap(key_, move.key_);
+   std::swap(val_, move.val_);
+
+   return *this;
+}
+
+bool LMDB::Iterator::operator==(const Iterator &other) const
+{
+   if (this == &other)
+      return true;
+
+   {
+      bool a = isEOF();
+      bool b = other.isEOF();
+      if (a && b) return true;
+      if (a || b) return false;
+   }
+   
+   return key() == other.key();
+}
+
+void LMDB::Iterator::advance()
+{
+   checkOk();
+   
+   MDB_val mkey;
+   MDB_val mval;
+   
+   int rc = mdb_cursor_get(csr_, &mkey, &mval, MDB_NEXT);
+
+   if (rc == MDB_NOTFOUND)
+      has_ = false;
+   else if (rc != MDB_SUCCESS)
+      throw LMDBException("Failed to seek (" + errorString(rc) +")");
+   else
+   {
+      has_ = true;
+      key_ = std::string(static_cast<char*>(mkey.mv_data), mkey.mv_size);
+      val_ = std::string(static_cast<char*>(mval.mv_data), mval.mv_size);
+   }
+}
+
+void LMDB::Iterator::toFirst()
+{
+   checkHasDb();
+   
+   MDB_val mkey;
+   MDB_val mval;
+   
+   int rc = mdb_cursor_get(csr_, &mkey, &mval, MDB_FIRST);
+
+   if (rc == MDB_NOTFOUND)
+      has_ = false;
+   else if (rc != MDB_SUCCESS)
+      throw LMDBException("Failed to seek (" + errorString(rc) +")");
+   else
+   {
+      has_ = true;
+      key_ = std::string(static_cast<char*>(mkey.mv_data), mkey.mv_size);
+      val_ = std::string(static_cast<char*>(mval.mv_data), mval.mv_size);
+   }
+}
+
+void LMDB::Iterator::seek(const CharacterArrayRef &key, SeekBy e)
+{
+   checkHasDb();
+   
+   MDB_val mkey = { key.len, const_cast<char*>(key.data) };
+   MDB_val mval = {0, 0};
+
+   MDB_cursor_op op=MDB_SET;
+   if (e == Seek_GE)
+      op = MDB_SET_RANGE;
+   else if (e == Seek_LE)
+      op = MDB_SET_RANGE;
+
+   int rc = mdb_cursor_get(csr_, &mkey, &mval, op);
+   if (e == Seek_LE)
+   {
+      if (rc == MDB_NOTFOUND)
+         rc = mdb_cursor_get(csr_, &mkey, &mval, MDB_LAST);
+      // now make sure mkey is less than key
+      if (rc == MDB_NOTFOUND)
+      {
+         has_ = false;
+         return;
+      }
+      else if (rc != MDB_SUCCESS)
+         throw LMDBException("Failed to seek (" + errorString(rc) +")");
+      
+      if (mkey.mv_size > key.len)
+      {
+         // mkey can't possibly be before key if it's longer than key
+         has_ = false;
+         return;
+      }
+      const int cmp = std::memcmp(mkey.mv_data, key.data, key.len);
+      if (cmp <= 0)
+      {
+         // key is longer and the earlier bytes are the same,
+         // therefor, mkey is before key
+         has_ = true;
+         key_ = std::string(static_cast<char*>(mkey.mv_data), mkey.mv_size);
+         val_ = std::string(static_cast<char*>(mval.mv_data), mval.mv_size);
+         return;
+      }
+      else
+      {
+         has_ = false;
+         return;
+      }
+   }
+   
+   if (rc == MDB_NOTFOUND)
+      has_ = false;
+   else if (rc != MDB_SUCCESS)
+      throw LMDBException("Failed to seek (" + errorString(rc) +")");
+   else
+   {
+      has_ = true;
+      key_ = std::string(static_cast<char*>(mkey.mv_data), mkey.mv_size);
+      val_ = std::string(static_cast<char*>(mval.mv_data), mval.mv_size);
+   }
+}
+
+LMDB::Transaction::Transaction(LMDB *db)
+   : db(db), began(false)
+{
+   begin();
+}
+
+LMDB::Transaction::~Transaction()
+{
+   commit();
+}
+
+void LMDB::Transaction::begin()
+{
+   if (began)
+      return;
+   
+   began = true;
+   db->transactionLevel++;
+   
+   if (db->transactionLevel != 1)
+      return;
+   
+   int modef = 0;
+   if (db->mode == ReadOnly)
+      modef = MDB_RDONLY;
+      
+   int rc = mdb_txn_begin(db->env, nullptr, modef, &db->txn);
+   if (rc != MDB_SUCCESS)
+      throw LMDBException("Failed to create transaction (" + errorString(rc) +")");
+}
+
+void LMDB::Transaction::commit()
+{
+   if (!began)
+      return;
+
+   db->transactionLevel--;
+   began=false;
+   if (db->transactionLevel == 0)
+   {
+      int rc = mdb_txn_commit(db->txn);
+      
+      if (rc != MDB_SUCCESS)
+      {
+         throw LMDBException("Failed to close db tx (" + errorString(rc) +")");
+      }
+      db->txn=0;
+   }
+}
+
+void LMDB::Transaction::rollback()
+{
+   throw std::runtime_error("unimplemented");
+}
+
+
+LMDB::LMDB()
+{
+   env = nullptr;
+   dbi = 0;
+   txn = nullptr;
+}
+
+LMDB::~LMDB()
+{
+   close();
+}
+
+void LMDB::open(const char *filename, Mode mode)
+{
+   if (env)
+      throw std::logic_error("Database object already open (close it first)");
+
+   transactionLevel=0;
+   dbi = 0;
+   txn = 0;
+   
+   int rc;
+
+   this->mode = mode;
+   
+   int modef = 0;
+   if (mode == ReadOnly)
+      modef = MDB_RDONLY;
+
+   rc = mdb_env_create(&env);
+   if (rc != MDB_SUCCESS)
+      throw LMDBException("Failed to load mdb env (" + errorString(rc) + ")");
+
+   mdb_env_set_mapsize(env, 100 * 1024 * 1024 * 1024LL);
+   rc = mdb_env_open(env, filename, modef|MDB_NOSYNC|MDB_NOSUBDIR, 0600);
+   if (rc != MDB_SUCCESS)
+      throw LMDBException("Failed to open db " + std::string(filename) + " (" + errorString(rc) + ")");
+
+   MDB_txn *txn;
+   rc = mdb_txn_begin(env, nullptr, modef, &txn);
+   if (rc != MDB_SUCCESS)
+      throw LMDBException("Failed to create transaction (" + errorString(rc) +")");
+
+   rc = mdb_dbi_open(txn, 0, 0, &dbi);
+   rc = mdb_txn_commit(txn);
+   if (rc != MDB_SUCCESS)
+   {
+      // cleanup here
+      throw LMDBException("Failed to close db tx (" + errorString(rc) +")");
+   }
+}
+
+void LMDB::close()
+{
+   if (env)
+   {
+      mdb_close(env, dbi);
+      mdb_env_close(env);
+      env = nullptr;
+   }
+}
+
+void LMDB::insert(
+   const CharacterArrayRef& key,
+   const CharacterArrayRef& value
+)
+{
+   MDB_val mkey = { key.len, const_cast<char*>(key.data) };
+   MDB_val mval = { value.len, const_cast<char*>(value.data) };
+   
+   if (!txn)
+      throw LMDBException("Failed to insert: need transaction");
+   
+   int rc = mdb_put(txn, dbi, &mkey, &mval, 0);
+   if (rc != MDB_SUCCESS)
+      throw LMDBException("Failed to insert (" + errorString(rc) + ")");
+}
+
+void LMDB::erase(const CharacterArrayRef& key)
+{
+   MDB_val mkey = { key.len, const_cast<char*>(key.data) };
+   int rc = mdb_del(txn, dbi, &mkey, 0);
+   if (rc != MDB_SUCCESS && rc != MDB_NOTFOUND)
+      throw LMDBException("Failed to erase (" + errorString(rc) + ")");
+}
+
+std::string LMDB::value(const CharacterArrayRef& key) const
+{
+   Iterator c = find(key);
+   if (!c.isValid())
+      throw NoValue("No such value with specified key");
+   
+   return c.value();
+}
+
+// kate: indent-width 3; replace-tabs on;
---
+++ cppForSwig/lmdbpp.h
@@ -0,0 +1,234 @@
+#ifndef LMDBPP_H
+#define LMDBPP_H
+
+#include <string>
+#include <stdexcept>
+#include <vector>
+#include <pthread.h>
+
+
+struct MDB_env;
+struct MDB_txn;
+struct MDB_cursor;
+
+// this exception is thrown for all errors from LMDB
+class LMDBException : public std::runtime_error
+{
+public:
+   LMDBException(const std::string &what)
+      : std::runtime_error(what)
+   { }
+};
+
+class NoValue : public LMDBException
+{
+public:
+   NoValue(const std::string &what)
+      : LMDBException(what)
+   { }
+
+};
+
+// a class that stores a pointer to a memory block
+class CharacterArrayRef
+{
+public:
+   const size_t len;
+   const char *data;
+   
+   CharacterArrayRef(const size_t len, const char *data)
+      : len(len), data(data)
+   { }
+   CharacterArrayRef(const size_t len, const unsigned char *data)
+      : len(len), data(reinterpret_cast<const char*>(data))
+   { }
+   CharacterArrayRef(const std::string &data)
+      : len(data.size()), data(&data[0])
+   { }
+   CharacterArrayRef(const std::vector<char> &data)
+      : len(data.size()), data(&data.front())
+   { }
+};
+
+
+class LMDB
+{
+   MDB_env *env;
+   // exists only with a Tx
+   unsigned int dbi;
+   MDB_txn *txn;
+   
+   unsigned transactionLevel;
+   
+   
+public:
+   // this class can be used like a C++ iterator,
+   // or you can just use isValid() to test for "last item"
+   class Iterator
+   {
+      friend class LMDB;
+      
+      const LMDB *db_=nullptr;
+      MDB_cursor *csr_=nullptr;
+         
+      bool has_=false;
+      std::string key_, val_;
+         
+      void reset();
+      void checkHasDb() const;
+      void checkOk() const;
+      void detach();
+      
+      Iterator(const LMDB *db);
+      
+   public:
+      Iterator() { }
+      ~Iterator();
+      
+      // copying permitted (encouraged!)
+      Iterator(const Iterator &copy);
+      Iterator(Iterator &&move);
+      Iterator& operator=(const Iterator &copy);
+      Iterator& operator=(Iterator &&move);
+      
+      // Returns true if the key pointed to is identical, or if both iterators
+      // are invalid, and false otherwise.
+      // returns true if the key pointed to is in different databases
+      bool operator==(const Iterator &other) const;
+      // the inverse
+      bool operator!=(const Iterator &other) const
+      {
+         return !operator==(other);
+      }
+      
+      enum SeekBy
+      {
+         Seek_EQ,
+         Seek_GE,
+         Seek_LE
+      };
+      
+      // move this iterator such that, if the exact key is not found:
+      // for e == Seek_EQ
+      // The cursor is left as Invalid.
+      // for e == Seek_GE
+      // The cursor is left pointing to the smallest key in the database that is
+      // larger than (key). If the database contains no keys larger than
+      // (key), the cursor is left as Invalid.
+      void seek(const CharacterArrayRef &key, SeekBy e = Seek_EQ);
+      
+      // is the cursor pointing to a valid location?
+      bool isValid() const { return has_; }
+      operator bool() const { return isValid(); }
+      bool isEOF() const { return !isValid(); }
+
+      // advance the cursor
+      // the postfix increment operator is not defined for performance reasons
+      Iterator& operator++() { advance(); return *this; }
+      void advance();
+      // advance this iterator "count" times
+      //void advance(int count);
+      
+      // seek this iterator to the first sequence
+      void toFirst();
+      
+      // returns the key currently pointed to, if no key is being pointed to
+      // std::logic_error is returned (not LSMException). LSMException may
+      // be thrown for other reasons. You can avoid logic_error by
+      // calling isValid() first
+      const std::string& key() const { return key_; }
+      
+      // returns the value currently pointed to. Exceptions are thrown
+      // under the same conditions as key()
+      const std::string& value() const { return val_; }
+   };
+   
+   class Transaction
+   {
+      LMDB *db;
+      bool began;
+   public:
+      // begin a transaction
+      Transaction(LMDB *db);
+      // commit a transaction if it exists
+      ~Transaction();
+      
+      // commit a transaction, if it exists, doing nothing otherwise.
+      // after this function completes, no transaction exists
+      void commit();
+      // rollback the transaction, if it exists, doing nothing otherwise.
+      // All modifications made since this transaction began are removed.
+      // After this function completes, no transaction exists
+      void rollback();
+      // start a new transaction. If one already exists, do nothing
+      void begin();
+   private:
+      Transaction(const Transaction&); // no copies
+   };
+
+   LMDB();
+   ~LMDB();
+   
+   enum Mode
+   {
+      ReadOnly,
+      ReadWrite
+   };
+   
+   // open a database by filename
+   void open(const char *filename, Mode mode=ReadWrite);
+   void open(const std::string &filename, Mode mode=ReadWrite)
+      { open(filename.c_str(), mode); }
+
+   // close a database, doing nothing if one is presently not open
+   void close();
+   
+   // insert a value into the database, replacing
+   // the one with a matching key if it is already there
+   void insert(
+      const CharacterArrayRef& key,
+      const CharacterArrayRef& value
+   );
+   
+   // delete the entry with the given key, doing nothing
+   // if such a key does not exist
+   void erase(const CharacterArrayRef& key);
+
+   // read the value having the given key
+   std::string value(const CharacterArrayRef& key) const;
+   
+   // create a cursor for scanning the database that points to the first
+   // item
+   Iterator begin() const
+   {
+      Iterator c(this);
+      c.toFirst();
+      return c;
+   }
+   // creates a cursor that points to an invalid item
+   Iterator end() const
+   {
+      Iterator c(this);
+      return c;
+   }
+   
+   template<class T>
+   Iterator find(const T &t) const
+   {
+      Iterator c = cursor();
+      c.seek(t);
+      return c;
+   }
+   
+   // Create an iterator that points to an invalid item.
+   // like end(), the iterator can be repositioned to
+   // become a valid entry
+   Iterator cursor() const { return end(); }
+private:
+   LMDB(const LMDB &nocopy);
+   Mode mode;
+};
+
+#endif
+// kate: indent-width 3; replace-tabs on;
+
