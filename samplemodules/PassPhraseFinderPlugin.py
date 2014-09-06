51cc7a914c06ccda377c9d3f8810cb0725b14dd2
AndyOfiesh <andy@bitcoinarmory.com>
2014-09-06 19:04:27 -0400

    ARMORY-174 Create a plugin for finding a forgotten passphrase Finished implementation

--- samplemodules/PassPhraseFinderPlugin.py
+++ samplemodules/PassPhraseFinderPlugin.py
@@ -4,16 +4,18 @@ import os
 from PyQt4.Qt import QPushButton, SIGNAL, QTextEdit, QScrollArea, QTabWidget, \
    QLineEdit, QAbstractTableModel, QModelIndex, Qt, QVariant, QTableView, QIcon,\
    QDialogButtonBox, QGridLayout, QLabel, QComboBox, QMenu, QCursor, QListWidget,\
-   QListWidgetItem, QMessageBox
+   QListWidgetItem, QMessageBox, QString
 
 from CppBlockUtils import SecureBinaryData
 from armoryengine.ArmoryUtils import RightNow, script_to_addrStr, \
-   addrStr_to_hash160, enum, isASCII
+   addrStr_to_hash160, enum, isASCII, PyBackgroundThread
 from armoryengine.PyBtcWallet import PyBtcWallet
 from qtdefines import QRichLabel, makeHorizFrame, GETFONT, relaxedSizeNChar, \
    makeVertFrame, tightSizeNChar, initialColResize, ArmoryDialog,\
    UnicodeErrorBox
 from armorycolors import Colors
+from armorymodels import WLTVIEWCOLS
+from PyQt4 import QtGui
 
 
 # Give an upper limit for any method to return
@@ -27,7 +29,8 @@ MAX_SEGMENT_LENGTH = 20
 MAX_SEGMENTS = 20
 MAX_UNKNOWN_SEGMENT_LENGTH = 10
 
-
+BILLION_INT = 1000000000
+OVER_BILLION_STR = '> 1 Billion'
 
 class PluginObject(object):
 
@@ -36,10 +39,71 @@ class PluginObject(object):
    
    #############################################################################
    def __init__(self, main):
-
-      def searchPassPhrase():
-         pass
+      self.searchThread = None
+      self.passPhraseFinder = None
+      self.isSearchOver = False
       
+      def updateResults(resultStr):
+         self.resultStr += resultStr
+         
+      def updateDisplay():
+         if len(self.resultStr) > 0:
+            self.resultsDisplay.append(self.resultStr)
+            self.resultStr = ''
+            self.resultsDisplay.moveCursor(QtGui.QTextCursor.End)
+            self.resultsDisplay.repaint()
+         if self.isSearchOver:
+            endSearch()
+      
+      # Call this from another thread to end the search
+      def terminateSearch():
+         self.isSearchOver = True
+      
+      # Call this from the main thread to end the search
+      def endSearch():
+         self.main.extraHeartbeatAlways.remove(updateDisplay)
+         self.searchButton.setEnabled(True)
+         self.stopButton.setEnabled(False)
+         # If the thread is still searching tell the pass phrase finder to stop
+         if self.passPhraseFinder and self.searchThread  and not self.searchThread.isFinished():
+            self.passPhraseFinder.isStopped = True
+            
+      def searchForPassphrase():
+         # Get the selected wallet from the main screen
+         wlt = self.getSelectedWlt()
+         if wlt and not wlt.watchingOnly and wlt.isLocked:
+            self.resultStr = ''
+            self.passPhraseFinder = PassPhraseFinder(wlt)
+            self.resultsDisplay.setText(QString(''))
+            self.main.extraHeartbeatAlways.append(updateDisplay)
+            if len(self.segOrdStrSet) > 0:
+               # From self.segOrdStrList, create a list of lists of indexes that describe the segment orderings to search
+               # In other words convert all of the strings in orderings list to lists of integers
+               segOrdIntListList = []
+               for ordStr in self.segOrdStrSet:
+                  # The indexes provided by the users are 1 based, and the list indexes ought to be 0 based
+                  segOrdIntListList.append([int(indexStr)-1 for indexStr in ordStr.split(',')])
+               self.searchThread = PyBackgroundThread(self.passPhraseFinder.searchForPassPhrase, 
+                  [segDef.getSegList() for segDef in self.segDefList], 
+                  segOrdIntListList, 
+                  updateResults,
+                  terminateSearch )
+               # Reset the isSearchOver flag
+               self.isSearchOver = False
+               self.searchThread.start()
+               
+               # Disable search button adn enabled stop button
+               self.stopButton.setEnabled(True)
+               self.searchButton.setEnabled(False)
+            else:
+               QMessageBox.warning(self.main, tr('Invalid'), tr("""
+                  There are no valid segment combinations to search.
+                  Please add at least one segment and ordering to search."""), QMessageBox.Ok)
+         else:
+            QMessageBox.warning(self.main, tr('Invalid'), tr("""
+               No valid wallet is selected. Please select a locked
+               non-watching-only from Available Wallets."""), QMessageBox.Ok)
+
       def addKnownSegment():
          dlgEnterSegment = DlgEnterSegment(main, main)
          if dlgEnterSegment.exec_():
@@ -70,16 +134,16 @@ class PluginObject(object):
          if len(self.segDefList) > 0:
             dlgSpecifyOrdering = DlgSpecifyOrdering(main, main, len(self.segDefList))     
             if dlgSpecifyOrdering.exec_():
-               self.orderingListBox.addItem(QListWidgetItem(str(dlgSpecifyOrdering.editOrdering.text())))
+               self.segOrdStrSet.add(str(dlgSpecifyOrdering.parseOrderingList()).strip('[]'))
+               self.updateOrderingListBox()
          else:
             QMessageBox.warning(self.main, tr('Not Ready'), tr("""
                No segments have been entered. You must enter some segments before you can order them."""), QMessageBox.Ok)
-      
-      def searchForPassphrase():
-         pass
+
       
       self.main = main
       self.segDefList = []
+      self.segOrdStrSet = set()
       segmentHeader = QRichLabel(tr("""<b>Build segments for pass phrase search: </b>"""), doWrap=False)
       self.knownButton = QPushButton("Add Known Segment")
       self.unknownCaseButton = QPushButton("Add Unknown Case Segment")
@@ -99,36 +163,60 @@ class PluginObject(object):
       
       h = tightSizeNChar(self.segDefTableView, 1)[1]
       self.segDefTableView.setMinimumHeight(2 * (1.3 * h))
-      self.segDefTableView.setMaximumHeight(10 * (1.3 * h))
-      
-      self.segDefTableView.customContextMenuRequested.connect(self.showContextMenu)
-      
+      self.segDefTableView.setMaximumHeight(10 * (1.3 * h))      
       initialColResize(self.segDefTableView, [.1, .2, .4, .1, .1, .1])
 
+      self.segDefTableView.customContextMenuRequested.connect(self.showSegContextMenu)
       self.segDefTableView.setContextMenuPolicy(Qt.CustomContextMenu)
-
       
       segmentOrderingsHeader = QRichLabel(tr("""<b>Specify orderings for pass phrase search: </b>"""), doWrap=False)
       self.addOrderingButton = QPushButton("Add Ordering")
       
       
       self.main.connect(self.addOrderingButton, SIGNAL('clicked()'), addOrdering)
-      orderingButtonPanel = makeHorizFrame([self.addOrderingButton, 'stretch'])
+      orderingButtonPanel = makeHorizFrame([segmentOrderingsHeader, self.addOrderingButton, 'stretch'])
 
-      self.orderingListBox  = QListWidget()
+      self.segOrdListBox  = QListWidget()
+      
+      self.segOrdListBox.customContextMenuRequested.connect(self.showOrdContextMenu)
+      self.segOrdListBox.setContextMenuPolicy(Qt.CustomContextMenu)
+      
       
       self.searchButton = QPushButton("Search")
       self.main.connect(self.searchButton, SIGNAL('clicked()'), searchForPassphrase)
-      searchButtonPanel = makeHorizFrame([self.searchButton, 'stretch'])
-
+      self.stopButton = QPushButton("Stop Searching")
+      self.stopButton.setEnabled(False)
+      self.main.connect(self.stopButton, SIGNAL('clicked()'), endSearch)
+      totalSearchLabel = QRichLabel(tr("""<b>Total Passphrase Tries To Search: </b>"""), doWrap=False)
+      self.totalSearchTriesDisplay = QLineEdit()
+      self.totalSearchTriesDisplay.setReadOnly(True)
+      self.totalSearchTriesDisplay.setText(QString('0'))
+      self.totalSearchTriesDisplay.setFont(GETFONT('Fixed'))
+      self.totalSearchTriesDisplay.setMinimumWidth(tightSizeNChar(self.totalSearchTriesDisplay, 6)[0])
+      self.totalSearchTriesDisplay.setMaximumWidth(tightSizeNChar(self.totalSearchTriesDisplay, 12)[0])
+      searchButtonPanel = makeHorizFrame([self.searchButton, self.stopButton, 'stretch', totalSearchLabel,  self.totalSearchTriesDisplay])
+      
+      self.resultsDisplay = QTextEdit()
+      self.resultsDisplay.setReadOnly(True)
+      self.resultsDisplay.setFont(GETFONT('Fixed'))
+      self.resultsDisplay.setMinimumHeight(100)
       self.searchPanel = makeVertFrame([topRow, self.segDefTableView, orderingButtonPanel,
-             self.orderingListBox, searchButtonPanel, 'stretch' ])
+             self.segOrdListBox, searchButtonPanel, self.resultsDisplay, 'stretch'])
       # Now set the scrollarea widget to the layout
       self.tabToDisplay = QScrollArea()
       self.tabToDisplay.setWidgetResizable(True)
       self.tabToDisplay.setWidget(self.searchPanel)
    
-   def showContextMenu(self):
+   def getSelectedWlt(self):
+      wlt = None
+      selectedWltList = self.main.walletsView.selectedIndexes()
+      if len(selectedWltList)>0:
+            row = selectedWltList[0].row()
+            wltID = str(self.main.walletsView.model().index(row, WLTVIEWCOLS.ID).data().toString())
+            wlt = self.main.walletMap[wltID]
+      return wlt
+   
+   def showSegContextMenu(self):
       menu = QMenu(self.segDefTableView)
       if len(self.segDefTableView.selectedIndexes())==0:
          return
@@ -138,18 +226,56 @@ class PluginObject(object):
       action = menu.exec_(QCursor.pos())
       
       if action == deleteSegMenuItem:
-         self.deleteRow(row)
+         self.deleteSegRow(row)
+         
+   def showOrdContextMenu(self):
+      menu = QMenu(self.segOrdListBox)
+      if len(self.segOrdListBox.selectedItems())==0:
+         return
+
+      item = self.segOrdListBox.currentItem()
+      deleteOrdMenuItem = menu.addAction("Delete Ordering")
+      action = menu.exec_(QCursor.pos())
       
-   def deleteRow(self, row):
+      if action == deleteOrdMenuItem:
+         self.deleteOrdItem(item)
+         
+   def deleteSegRow(self, row):
       self.segDefList.remove(self.segDefList[row])
       self.segDefTableModel.updateSegList(self.segDefList)
+      self.segOrdStrSet.clear()
+      self.updateOrderingListBox()
+   
+         
+   def deleteOrdItem(self, ordItem):
+      ordText = str(ordItem.text())
+      self.segOrdStrSet.remove(ordText)
+      self.updateOrderingListBox()
    
    
    def getTabToDisplay(self):
       return self.tabToDisplay
 
-
-
+   def updateOrderingListBox(self):
+      self.segOrdListBox.clear()
+      segOrdList = list(self.segOrdStrSet)
+      segOrdList.sort()
+      totalTries = 0
+      for ordStr in segOrdList:
+         self.segOrdListBox.addItem(QListWidgetItem(ordStr))
+         totalTries += self.calculateTries(ordStr)
+      if totalTries > BILLION_INT:
+         self.totalSearchTriesDisplay.setText(OVER_BILLION_STR)
+      else:         
+         self.totalSearchTriesDisplay.setText(str(totalTries))
+      
+   def calculateTries(self, ordStr):
+      ordIntList = [int(indexStr) for indexStr in ordStr.split(',')]
+      totalTries = 1
+      # Multiply each of the totals of segment instance together.
+      for ordInt in ordIntList:
+         totalTries *= self.segDefList[ordInt-1].getSegListLen()
+      return totalTries
    
 class DlgSpecifyOrdering(ArmoryDialog):
 
@@ -177,16 +303,18 @@ class DlgSpecifyOrdering(ArmoryDialog):
       self.setLayout(layout)
    
    # return empty list if not a valid list of numbers
-   def parseOrderingList(self, theString):
+   def parseOrderingList(self):
       try:
-         return [int(i) for i in theString.split(',')]
+         return [int(i) for i in str(self.editOrdering.text()).split(',')]
       except:
          return []
       
    #############################################################################
    def accept(self):
-      orderingList = self.parseOrderingList(str(self.editOrdering.text())) 
+      orderingList = self.parseOrderingList() 
       if len(orderingList) < 1:
+         QMessageBox.warning(self.main, tr('Invalid'), tr("""
+            Some segment indices are invalid."""), QMessageBox.Ok)
          return
       for segIndex in orderingList:
          if segIndex > self.maxSeg or segIndex < 1:
@@ -230,7 +358,7 @@ class DlgEnterSegment(ArmoryDialog):
             self.maxSelector.addItem(str(i))
          # default to 1 to 4
          self.minSelector.setCurrentIndex(0)
-         self.maxSelector.setCurrentIndex(3)
+         self.maxSelector.setCurrentIndex(0)
          
          # fix the inversion of min and max when user sets min
          def updateMaxSelector():
@@ -333,9 +461,12 @@ class UnknownSeg(PwdSeg):
       self.removeDups()
       self.minLen = minLen
       self.maxLen = maxLen
-      
+   
+   # Crate a sorted list of unique characters from the user entry
    def removeDups(self):
-      self.known = ''.join(set(self.known))
+      uniqueCharList = list(set(self.known))
+      uniqueCharList.sort()
+      self.known = ''.join(uniqueCharList)
       
    def segListRecursiveGenerator(self, segLen):
       if segLen > 0:
@@ -361,82 +492,80 @@ class UnknownSeg(PwdSeg):
 
 
 
-class PasswordFinder(object): 
-   def __init__(self, wallet=None, walletPath=''):
-      if wallet != None:
-         self.wallet = wallet
-      else:
-         if not os.path.exists(walletPath):
-            print 'Wallet does not exist:'
-            print '  ', walletPath
-            raise WalletNotFound
-         self.wallet = PyBtcWallet().readWalletFile(walletPath)
+class PassPhraseFinder(object): 
+   def __init__(self, wallet):
+      self.wallet = wallet
+      self.isStopped = False
 
-   def countPasswords(self, segList, segOrdList):
+
+   def countPassPhrases(self, segList, segOrdList):
       return reduce(add, [reduce(mul, [len(segList[segIndex])
                                        for segIndex in segOrd])
                           for segOrd in segOrdList])
    
-   def recursivePasswordGenerator(self, segList):
+   def recursivePassPhraseGenerator(self, segList):
       if len(segList) > 0:
          for a in segList[0]:
-            for b in self.recursivePasswordGenerator(segList[1:]):
+            for b in self.recursivePassPhraseGenerator(segList[1:]):
                yield a + b
       else:
          yield ''
          
-   # Generates passwords from segs in segList
+   # Generates PassPhrases from segs in segList
    #     Example Input: [['Andy','b','c'],['1','2'],['!']]
    # The segOrdList contains a list of ordered 
    # permutations of the segList:
    #     Example Input: [[0,1,2],[2,0,1,],[0,1]]
-   # Yields one password at a time until all permutations are exhausted
+   # Yields one PassPhrase at a time until all permutations are exhausted
    #     Example: Andy1!, Andy2!, b1!, b2!, c1!, c2!,
    #              !Andy1, !Andy2, !b1, !b2, !c1, !c2,
    #              Andy1, Andy2, b1, b2, c1, c2
    # The above example is a test case found in test/FindPassTest.py
-   def passwordGenerator(self, segList, segOrdList):
+   def passPhraseGenerator(self, segList, segOrdList):
       for segOrd in segOrdList:
          orderedSegList = [segList[segIndex] for segIndex in segOrd]
-         for result in self.recursivePasswordGenerator(orderedSegList):
+         for result in self.recursivePassPhraseGenerator(orderedSegList):
             yield result
    
-   def searchForPassword(self, segList, segOrdList=[]):
+   def searchForPassPhrase(self, segList, segOrdList, outputCallback, endCallback):
+      # if segOrdList is nonempty seglist must be non-empty too
       if len(segOrdList) == 0:
-         segOrdList = [range(len(segList))]
-      passwordCount = self.countPasswords(segList, segOrdList)
+         QMessageBox.warning(self.main, tr('Invalid'), tr("""
+            No segment orderings have been specified."""), QMessageBox.Ok)
+      passPhraseCount = self.countPassPhrases(segList, segOrdList)
       startTime = RightNow()
       found = False
       result = None
-      for i,p in enumerate(self.passwordGenerator(segList, segOrdList)):
+      outputLine = ''
+      for i,p in enumerate(self.passPhraseGenerator(segList, segOrdList)):
          isValid = self.wallet.verifyPassphrase( SecureBinaryData(p) ) 
             
          if isValid:
             # If the passphrase was wrong, it would error out, and not continue
-            print 'Passphrase found!'
-            print ''
-            print '\t', p
-            print ''
-            print 'Thanks for using this script.  If you recovered coins because of it, '
-            print 'please consider donating :) '
-            print '   1ArmoryXcfq7TnCSuZa9fQjRYwJ4bkRKfv'
-            print ''
-            found = True
-            open('FOUND_PASSWORD.txt','w').write(p)
+            outputCallback(outputLine)
+            outputCallback('\nPassphrase found!\n')
+            outputCallback('\tThe Pass Phrase for ' + self.wallet.labelName + ' is: ' + p + '\n')
+            outputCallback('Thanks for using this script.  If you recovered coins because of it, \n')
+            outputCallback('please consider donating :) \n')
+            outputCallback('   1ArmoryXcfq7TnCSuZa9fQjRYwJ4bkRKfv\n')
             result = p
+            found = True
             break
          elif i%100==0:
-               telapsed = (RightNow() - startTime)/3600.
-               print ('%d/%d passphrases tested... (%0.1f hours so far)'%(i,passwordCount,telapsed)).rjust(40)
-         print p,
+            telapsed = (RightNow() - startTime)/3600.
+            outputCallback( ('\n%d/%d passphrases tested... (%0.1f hours so far)'%(i,passPhraseCount,telapsed)).rjust(40))
+         outputLine += p
          if i % 10 == 9:
-            print
+            outputCallback(outputLine)
+            outputLine = ''
+         else:
+            outputLine += ' '
+         if self.isStopped:
+            return result
       if not found:
-         print ''
-         
-         print 'Script finished!'
-         print 'Sorry, none of the provided passphrases were correct :('
-         print ''
+         outputCallback('Script finished!\n')
+         outputCallback('Sorry, none of the provided passphrases were correct :(')
+      endCallback()
       return result
 
 
@@ -468,9 +597,15 @@ class SegDefDisplayModel(QAbstractTableModel):
                 'Unknown Case' if isinstance(segDef, UnknownCaseSeg) else \
                 'Unknown Order'
       segText = segDef.known
-      segMinLength = segDef.minLen if segDef is UnknownSeg else len(segText)
-      segMaxLength = segDef.maxLen if segDef is UnknownSeg else len(segText)
+      if isinstance(segDef, UnknownSeg):
+         segMinLength = segDef.minLen
+         segMaxLength = segDef.maxLen
+      else:
+         segMinLength = len(segText)
+         segMaxLength = len(segText)
       totalCombinations = segDef.getSegListLen()
+      if totalCombinations > BILLION_INT:
+         totalCombinations = OVER_BILLION_STR
       
       if role==Qt.DisplayRole:
          if col==SEGDEFCOLS.index: return QVariant(row+1)
