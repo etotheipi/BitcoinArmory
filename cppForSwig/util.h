ef4d31045d994ac1bd6d580d0e9dec874a2dc7e0
Charles Samuels <charles@bitcoinarmory.com>
2014-10-28 10:21:09 -0700

    add a readwritelock to util.h

--- cppForSwig/util.h
+++ cppForSwig/util.h
@@ -1,6 +1,9 @@
 #ifndef UTIL_H
 #define UTIL_H
 
+#include <mutex>
+#include <condition_variable>
+
 template<class Container>
 class IterateSecond
 {
@@ -82,6 +85,90 @@ public:
 template<class Container>
 inline IterateSecond<Container> values(Container &c)
    { return IterateSecond<Container>(c); }
+
+
+class ReadWriteLock
+{
+   std::mutex all_lock;
+   unsigned num_readers=0, wants_writers=0;
+   std::condition_variable no_readers, no_writers;
+   
+public:
+   void lockRead()
+   {
+      unique_lock<mutex> rl(all_lock);
+      while (wants_writers > 0)
+         no_writers.wait(rl);
+      num_readers++;
+   }
+   void unlockRead()
+   {
+      unique_lock<mutex> rl(all_lock);
+      num_readers--;
+      no_readers.notify_all();
+   }
+   
+   void lockWrite()
+   {
+      unique_lock<mutex> rl(all_lock);
+      wants_writers++;
+      while (num_readers > 0)
+         no_readers.wait(rl);
+      wants_writers--;
+      
+      rl.release();
+   }
+   
+   void unlockWrite()
+   {
+      no_writers.notify_all();
+      all_lock.unlock();
+   }
+
+   class ReadLock
+   {
+      ReadWriteLock *const l;
+      bool locked=true;
+   public:
+      ReadLock(ReadWriteLock &rwl)
+         : l(&rwl)
+      {
+         l->lockRead();
+      }
+      void unlock()
+      {
+         locked=false;
+         l->unlockRead();
+      }
+      ~ReadLock()
+      {
+         if (locked)
+            l->unlockRead();
+      }
+   };
+   class WriteLock
+   {
+      ReadWriteLock *const l;
+      bool locked=true;
+   public:
+      WriteLock(ReadWriteLock &rwl)
+         : l(&rwl)
+      {
+         l->lockWrite();
+      }
+      void unlock()
+      {
+         locked=false;
+         l->unlockRead();
+      }
+      ~WriteLock()
+      {
+         if (locked)
+            l->unlockWrite();
+      }
+   };
+   
+};
 
 #endif
 
