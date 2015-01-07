d423d4cfbdef925ea71c4ba7aaa6e182e4d2bb10
Jimmy Song <jaejoon@gmail.com>
2015-01-07 17:16:31 -0600

    Made update_version a little more multi-platform by using the .git directory structure

--- update_version.py
+++ update_version.py
@@ -1,20 +1,26 @@
-from subprocess import check_output, CalledProcessError
-
 import os
 
 
-try:
-    # check if the build file exists
+if os.path.exists('.git') \
+   and os.path.exists(os.path.join("armoryengine", "ArmoryUtils.py")):
+    current_head = os.path.join(".git", "HEAD")
+    f = open(current_head, "r")
+    ref = f.read()
+    f.close()
+    path_parts = ref[5:-1].split("/")
+    hash_loc = os.path.join(".git", *path_parts)
+    f = open(hash_loc, "r")
+    build = f.read()[:10]
+    f.close()
+
     build_file = os.path.join("armoryengine", "ArmoryBuild.py")
-    if os.path.exists('.git') \
-       and os.path.exists(os.path.join("armoryengine", "ArmoryUtils.py")):
-        build = check_output(["git", "rev-parse", "HEAD"])[:10]
-        f = open(build_file, "w")
-        f.write("BTCARMORY_BUILD = '%s'\n" % build)
-        f.close()
-        print "Build number has been updated to %s" % build
-    else:
-        print "Please run this script from the root Armory source directory"
-except CalledProcessError:
-    print "\nPlease update the build version when using the git source tree"
+    f = open(build_file, "w")
+    f.write("BTCARMORY_BUILD = '%s'\n" % build)
+    f.close()
+
+    print "Build number has been updated to %s" % build
+
+else:
+    print "Please run this script from the root Armory source directory" \
+        " along with the .git directory"
 
