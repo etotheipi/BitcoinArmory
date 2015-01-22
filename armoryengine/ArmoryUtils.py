0314f5ae2fd212684f8a51aac7ecf7adb9dda896
Alan Reiner <alan@bitcoinarmory.com>
2015-01-21 21:04:06 -0500

    Incremented version to 0.92.99.3-testing

--- armoryengine/ArmoryUtils.py
+++ armoryengine/ArmoryUtils.py
@@ -65,7 +65,7 @@ DEFAULT = 'DEFAULT'
 
 
 # Version Numbers 
-BTCARMORY_VERSION    = (0, 92, 99, 2)  # (Major, Minor, Bugfix, AutoIncrement) 
+BTCARMORY_VERSION    = (0, 92, 99, 3)  # (Major, Minor, Bugfix, AutoIncrement) 
 PYBTCWALLET_VERSION  = (1, 35,  0, 0)  # (Major, Minor, Bugfix, AutoIncrement)
 
 ARMORY_DONATION_ADDR = '1ArmoryXcfq7TnCSuZa9fQjRYwJ4bkRKfv'
