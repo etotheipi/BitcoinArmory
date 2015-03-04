54b96eebb7d946d27356561c61ed7b3f4acf95d4
Joseph Bisch <joseph.bisch@gmail.com>
2015-03-04 17:27:55 -0500

    Initial depends commit
    Taken from Bitcoin Core. This is a series of Makefiles designed to build
    dependencies in a deterministic way.

---
+++ depends/packages/pyqt4.mk
@@ -0,0 +1,10 @@
+package=pyqt4
+$(package)_version=4.11.3
+$(package)_download_path=http://sourceforge.net/projects/pyqt/files/PyQt4/PyQt-$($(package)_version)
+$(package)_linux_file_name=PyQt-x11-gpl-$($(package)_version).tar.gz
+$(package)_darwin_file_name=PyQt-mac-gpl-$($(package)_version).tar.gz
+$(package)_mingw32_file_name=PyQt-win-gpl-$($(package)_version).tar.gz
+$(package)_linux_sha256_hash=853780dcdbe2e6ba785d703d059b096e1fc49369d3e8d41a060be874b8745686
+$(package)_darwin_sha256_hash=8b8bb3a2ef8b7368710e0bc59d6e94e1f513f7dbf10a3aaa3154f7b848c88b4d
+$(package)_mingw32_sha256_hash=aa25a2e11464fd2ff26e1a3ad514aae6d89a61bb03ce4746d255a82cf909225d
+$(package)_dependencies=python
