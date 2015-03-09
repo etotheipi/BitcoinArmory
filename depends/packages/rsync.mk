e0eeb2a3fe2f9ad9bb6a00667c2eb3549b21200a
Joseph Bisch <joseph.bisch@gmail.com>
2015-03-09 10:38:49 -0400

    Depends: Have rsync use DESTDIR instead of --prefix
    With DESTDIR, the results of the build end up where they are supposed
    to.

--- depends/packages/rsync.mk
+++ depends/packages/rsync.mk
@@ -5,7 +5,7 @@ $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=7de4364fcf5fe42f3bdb514417f1c40d10bbca896abe7e7f2c581c6ea08a2621
 
 define $(package)_config_cmds
-  $($(package)_autoconf) --prefix=$($(package)_staging_dir)
+  $($(package)_autoconf)
 endef
 
 define $(package)_build_cmds
@@ -13,5 +13,5 @@ define $(package)_build_cmds
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) install
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install
 endef
