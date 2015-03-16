29fca2c52016aeb516f5833fc7133f12d413293f
Joseph Bisch <joseph.bisch@gmail.com>
2015-03-16 15:27:54 -0400

    Depends: Pass our versions of tools to Makefiles
    These packages don't use autoconf. So by default, they don't know about
    our tools we built. Maybe create a $(MAKE_CUSTOM_TOOLS), so that we
    don't have to repeat this code in each package Makefile that needs it.

--- depends/packages/pyqt5.mk
+++ depends/packages/pyqt5.mk
@@ -7,12 +7,16 @@ $(package)_sha256_hash=c6c33f392c0a2b0c9ec16531dc88fa9aee853e9fff10f0ad9d0e47776
 $(package)_mingw32_sha256_hash=39fe23844b08f67833581f98dbe5cf9de501785a0af1176e3e8046a9fb28a247
 $(package)_dependencies=python3 qt sip gcc
 
+define $(package)_set_vars
+$(package)_build_opts=CC="$($(package)_cc)" CXX="$($(package)_cxx)" AR="$($(package)_ar)" NM="$($(package)_nm)" RANLIB="$($(package)_ranlib)"
+endef
+
 define $(package)_config_cmds
   $($(package)_prefixbin)python3 configure.py --confirm-license --sip=$($(package)_prefixbin)sip
 endef
 
 define $(package)_build_cmds
-  $(MAKE)
+  $(MAKE) $($(package)_build_opts)
 endef
 
 define $(package)_stage_cmds
