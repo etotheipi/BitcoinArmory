cbff6504a33c05d1daf02b041f77178bf94c53d5
Joseph Bisch <joseph.bisch@gmail.com>
2015-03-25 09:50:12 -0400

    Depends: Use DESTDIR instead of prefix for pcre

--- depends/packages/pcre.mk
+++ depends/packages/pcre.mk
@@ -4,10 +4,6 @@ $(package)_download_path=http://sourceforge.net/projects/$(package)/files/$(pack
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=b37544f33caed0cc502a1e729c3b1d3df5086dcc819b9125c30700c239246c9e
 
-define $(package)_set_vars
-$(package)_config_opts = --prefix $($(package)_staging_dir)
-endef
-
 define $(package)_config_cmds
   $($(package)_autoconf)
 endef
@@ -17,5 +13,5 @@ define $(package)_build_cmds
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) install
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install
 endef
