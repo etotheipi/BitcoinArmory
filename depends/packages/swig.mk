c9c7d4fa180300c88b834495661be63630bbaf9d
Joseph Bisch <joseph.bisch@gmail.com>
2015-03-25 18:22:03 -0400

    Depends: Undo last commit and disable ccache for swig build
    Last commit didn't work. We don't seem to need ccache-swig to build
    Armory, so I'm just going to try disabling it.

--- depends/packages/swig.mk
+++ depends/packages/swig.mk
@@ -4,7 +4,6 @@ $(package)_download_path=http://sourceforge.net/projects/swig/files/swig/$(packa
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=9f4cb9e8f213f041853646f58fe5e8428d63250d05f5c943b6fa759c77322a3c
 $(package)_dependencies=pcre zlib
-$(package)_patches=ccache_includedir.patch
 
 define $(package)_set_vars
 $(package)_config_opts = --without-allegrocl --without-chicken --without-clisp
@@ -13,13 +12,9 @@ $(package)_config_opts += --without-java --without-lua --without-mzscheme
 $(package)_config_opts += --without-ocaml --without-octave --without-perl5
 $(package)_config_opts += --without-php4 --without-pike --without-r
 $(package)_config_opts += --without-ruby --without-rxspencer --without-tcl
-$(package)_config_opts += --prefix $($(package)_staging_dir)
+$(package)_config_opts += --prefix $($(package)_staging_dir) --disable-ccache
 $(package)_config_opts += PCRE_CONFIG=$($(package)_prefixbin)/pcre-config
 $(package)_build_opts = LIBS="-lpcre -lz"
-endef
-
-define $(package)_preprocess_cmds
-  patch -p1 < $($(package)_patch_dir)/ccache_includedir.patch
 endef
 
 define $(package)_config_cmds
