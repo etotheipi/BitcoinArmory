530467bb7a1429dcf6c455b5a13fd071b062bd6b
Joseph Bisch <joseph.bisch@gmail.com>
2015-04-01 17:52:02 -0400

    Depends: Explicitly set compiler, cflags, etc. for Python modules

--- depends/packages/native_sip.mk
+++ depends/packages/native_sip.mk
@@ -5,8 +5,14 @@ $(package)_file_name=sip-$($(package)_version).tar.gz
 $(package)_sha256_hash=4caa8d52e4403bae5c4c64f44de03a2cfd0bb10b6d96c5fb771133516df9abbd
 $(package)_dependencies=native_python2
 
+define $(package)_set_vars
+  $(package)_config_opts=CC="$($(package)_cc)" CXX="$($(package)_cxx)" CFLAGS="$($(package)_cflags)"
+  $(package)_config_opts+=CXXFLAGS="$($(package)_cxxflags)" LFLAGS="$($(package)_ldflags)"
+  $(package)_config_opts+=LINK_SHLIB="$($(package)_cc)" LINK="$($(package)_cc)"
+endef
+
 define $(package)_config_cmds
-  $($(package)_prefixbin)/python configure.py
+  $($(package)_prefixbin)/python configure.py $($(package)_config_opts)
 endef
 
 define $(package)_build_cmds
--- depends/packages/twisted.mk
+++ depends/packages/twisted.mk
@@ -4,7 +4,12 @@ $(package)_download_path=https://pypi.python.org/packages/source/T/Twisted
 $(package)_file_name=Twisted-$($(package)_version).tar.bz2
 $(package)_sha256_hash=ac609262253057cf2aeb9dc049ba7877d646f31b4caef06a50189a023df46b51
 $(package)_dependencies=native_python2 native_zope.interface
+$(package)_build_env=CC="$($(package)_cc)" LD="$($(package)_cc)" CFLAGS="$($(package)_cflags)"
+
+define $(package)_build_cmds
+  $($(package)_prefix)/native/bin/python setup.py build
+endef
 
 define $(package)_stage_cmds
-  $($(package)_prefix)/native/bin/python setup.py install --prefix=$($(package)_staging_prefix_dir)
+  $($(package)_prefix)/native/bin/python setup.py install --root=$($(package)_staging_dir) --prefix=$($(package)_prefix)
 endef
