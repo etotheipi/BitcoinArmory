e18934eb486f8dc2ea8d3867fc55f63e10b80679
Joseph Bisch <joseph.bisch@gmail.com>
2015-06-22 18:30:39 -0400

    Depends: Update to same state as autotools-gitian

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,10 +1,14 @@
 # zip is currently broken. Add it back to packages when it is fixed.
-packages:=openssl pyqt4 twisted
-native_packages := native_pcre native_psutil native_python2 native_rsync native_sip native_swig native_zlib native_zope.interface
+packages:=openssl pyqt5 python3 twisted
+native_packages := native_pcre native_psutil native_python3 native_rsync native_sip native_swig native_zlib native_zope.interface
 
 qt_native_packages = native_protobuf
 qt_packages = qrencode protobuf
 
-qt_linux_packages= qt48 expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans libICE libSM
-qt_darwin_packages=qt48
-qt_mingw32_packages=qt48
+qt_linux_packages= qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
+qt_darwin_packages=qt
+qt_mingw32_packages=qt
+
+ifneq ($(build_os),darwin)
+darwin_native_packages=native_cctools
+endif
