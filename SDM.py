795fe4cc0e3f0616a82e9d46e7037ecd396380f2
aofiesh <andy@bitcoinarmory.com>
2015-06-24 22:54:14 -0400

    ARMORY-453 SDM.py looks in the wrong place for Core 0.10.2+ in auto-bitcoind  - Fixed

--- SDM.py
+++ SDM.py
@@ -433,6 +433,7 @@ class SatoshiDaemonManager(object):
 
       else:
          # In case this was a downloaded copy, make sure we traverse to bin/64 dir
+         searchPaths.extend([os.path.join(p, 'bin') for p in extraSearchPaths])
          if SystemSpecs.IsX64:
             searchPaths.extend([os.path.join(p, 'bin/64') for p in extraSearchPaths])
          else:
