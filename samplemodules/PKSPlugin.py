1226fedd9d2af7eba991618669bce80ff39f0fe0
Douglas Roark <doug@bitcoinarmory.com>
2015-07-16 15:24:45 -0400

    ARMORY-454
    - Fix a regression error from the previous checkin.
    - Make record writing calls more flexible.
    - File cleanup.

--- samplemodules/PKSPlugin.py
+++ samplemodules/PKSPlugin.py
@@ -7,15 +7,13 @@ from PyQt4.Qt import QPushButton, QScrollArea, SIGNAL, QLabel, QLineEdit, \
    QTextEdit
 from qtdefines import QRichLabel, makeVertFrame, makeHorizFrame, GETFONT, \
    relaxedSizeNChar, VERTICAL
-from qtdialogs import createAddrBookButton
 from ui.WalletFrames import SelectWalletFrame
 from armoryengine.BDM import getBDM
-from twisted.internet import reactor
 from armoryengine.ArmoryOptions import getTestnetFlag
 
+# Class name is required by the plugin framework.
 class PluginObject(object):
-   # PKS is a place holder - As are all labels in this dialog
-   tabName = 'PKS'
+   tabName = 'PMTA'
    maxVersion = '0.99'
 
    #############################################################################
@@ -23,143 +21,167 @@ class PluginObject(object):
       self.main = main
       self.wlt = None
 
-      ##########################################################################
-      ##### Display the conversion values based on the Coinbase API
-      headerLabel    = QRichLabel(tr("<b>PKS</b>"""), doWrap=False)
+      # Set up the GUI.
+      headerLabel    = QRichLabel(tr("<b>PMTA-related Functions</b>"""),
+                                  doWrap=False)
       addressLabel = QLabel('Choose wallet:')
-      self.frmSelectedWlt = SelectWalletFrame(main, main, 
-                     VERTICAL,
-                     selectWltCallback=self.setWallet)
-      self.pksButton     = QPushButton('Save PKS')
-      self.dnssecButton     = QPushButton('Save PKS && PMTA')
-      self.paymentRequestButton     = QPushButton('Payment Request')
-      paymentRequestLabel = QLabel('Payment Request:')
-      self.paymentRequestTextArea = QTextEdit()
-      self.paymentRequestTextArea.setFont(GETFONT('Fixed', 8))
-      w,h = relaxedSizeNChar(self.paymentRequestTextArea, 68)[0], int(12 * 8.2)
-      self.paymentRequestTextArea.setMinimumWidth(w)
-      self.paymentRequestTextArea.setMinimumHeight(h)
-      self.paymentRequestTextArea.setReadOnly(True)
-      self.clearButton     = QPushButton('Clear')
+      self.frmSelectedWlt = SelectWalletFrame(main, main, VERTICAL,
+                                              selectWltCallback=self.setWallet)
+      self.pksButton      = QPushButton('Save PKS Record')
+      self.pmtaButton     = QPushButton('Save PMTA Record')
+      self.payReqButton   = QPushButton('Payment Request')
+      payReqLabel         = QLabel('Payment Request:')
+      self.payReqTextArea = QTextEdit()
+      self.payReqTextArea.setFont(GETFONT('Fixed', 8))
+      w                   = relaxedSizeNChar(self.payReqTextArea, 68)[0]
+      h                   = int(12 * 8.2)
+      self.payReqTextArea.setMinimumWidth(w)
+      self.payReqTextArea.setMinimumHeight(h)
+      self.payReqTextArea.setReadOnly(True)
+      self.clearButton    = QPushButton('Clear')
 
-      # Qt GUI calls must occur on the main thread. We need to update the wallet
-      # once the BDM is ready. So, we register a signal with the main thread
-      # that can be used to call a function.
-      self.main.connect(self.main, SIGNAL('pluginNotify'), self.UpdateWallet)
+      # Qt GUI calls must occur on the main thread. We need to update the frame
+      # once the BDM is ready, so that the wallet balance is shown. To do this,
+      # we register a signal with the main thread that can be used to call an
+      # associated function.
+      self.main.connect(self.main, SIGNAL('bdmReadyPMTA'), self.bdmReady)
 
+      # Action for when the PKS button is pressed.
       def pksAction():
-         print "PKS Button Press"
          self.savePKSFile()
 
-      def dnssecButton():
-         print "PKS & PMTA Button Press"
-         self.savePKSFile()
+      # Action for when the PMTA button is pressed.
+      def pmtaAction():
          self.savePMTAFile()
 
-      def paymentRequestAction():
-         self.paymentRequestTextArea.setText("<Payment Request Blob>")
+      # Action for when the payment request button is pressed.
+      def prAction():
+         # TO BE COMPLETED
+         self.payReqTextArea.setText("<Payment Request Blob>")
 
+      # Action for when the clear text button is pressed.
       def clearTextArea():
-         self.paymentRequestTextArea.setText('')
+         self.payReqTextArea.setText('')
 
       self.main.connect(self.pksButton, SIGNAL('clicked()'), pksAction)
-      self.main.connect(self.dnssecButton, SIGNAL('clicked()'), dnssecButton)
-      self.main.connect(self.paymentRequestButton, SIGNAL('clicked()'), paymentRequestAction)
+      self.main.connect(self.pmtaButton, SIGNAL('clicked()'), pmtaAction)
+      self.main.connect(self.payReqButton, SIGNAL('clicked()'), prAction)
       self.main.connect(self.clearButton, SIGNAL('clicked()'), clearTextArea)
 
+      # Create the frame and set the scrollarea widget to the layout.
+      # self.tabToDisplay is required by the plugin framework.
       pluginFrame = makeVertFrame([headerLabel,
-                                   makeHorizFrame([addressLabel, 'Stretch']),
-                                   makeHorizFrame([self.frmSelectedWlt, 'Stretch']),
-                                   makeHorizFrame([self.pksButton, self.dnssecButton, self.paymentRequestButton, 'Stretch']),
-                                   paymentRequestLabel,
-                                   makeHorizFrame([self.paymentRequestTextArea,'Stretch']),
-                                   makeHorizFrame([self.clearButton, 'Stretch']),
+                                   makeHorizFrame([addressLabel,
+                                                   'Stretch']),
+                                   makeHorizFrame([self.frmSelectedWlt,
+                                                   'Stretch']),
+                                   makeHorizFrame([self.pksButton,
+                                                   self.pmtaButton,
+                                                   self.payReqButton,
+                                                   'Stretch']),
+                                   payReqLabel,
+                                   makeHorizFrame([self.payReqTextArea,
+                                                   'Stretch']),
+                                   makeHorizFrame([self.clearButton,
+                                                   'Stretch']),
                                    'Stretch'])
-
-      # Now set the scrollarea widget to the layout
       self.tabToDisplay = QScrollArea()
       self.tabToDisplay.setWidgetResizable(True)
       self.tabToDisplay.setWidget(pluginFrame)
 
-      # Register a BDM callback that can be called when the BDM is ready.
-      TheBDM.registerCppNotification(self.handleNotification)
+      # Register the BDM callback for when the BDM sends signals.
+      getBDM().registerCppNotification(self.handleBDMNotification)
 
 
-   def savePKSFile(self):
+   # Call for when we want to save a binary PKS record to a file. By default,
+   # all PKS flags will be false.
+   # INPUT:  PKS-related flags (bool) - See armoryengine/ConstructedScript.py
+   # OUTPUT: None
+   # RETURN: Final PKS record (PKSRecord)
+   def savePKSFile(self, isStatic = False, useCompr = False, use160 = False,
+                   isUser = False, isExt = False, chksumPres = False):
          defName = 'armory_%s.pks' % self.wlt.uniqueIDB58
          filePath = unicode(self.main.getFileSave(defaultFilename = defName))
 
-         if not len(filePath)>0:
-            print 'NOTHING SAVED!'
+         if not len(filePath) > 0:
             return
          else:
-            sbdPublicKey33 = SecureBinaryData(self.wlt.sbdPublicKey33)
-            sbdPublicKey65 = CryptoECDSA().UncompressPoint(sbdPublicKey33);
+            # Start with the wallet's uncompressed root key.
+            sbdPubKey33 = SecureBinaryData(self.wlt.sbdPublicKey33)
+            sbdPubKey65 = CryptoECDSA().UncompressPoint(sbdPubKey33)
 
             pathdir = os.path.dirname(filePath)
-            pathfn  = os.path.basename(filePath)
-
             if not os.path.exists(pathdir):
-               raise FileExistsError('Path for new wlt does not exist: %s', pathdir)
+               raise FileExistsError('Path for new PMTA record does not ' \
+                                     'exist: %s', pathdir)
 
-            if os.path.exists(filePath):
-               raise FileExistsError('File already exists, will not overwrite')
-
+            # Write the PKS record to the file, then return the record.
             myPKS = PublicKeySource()
-            myPKS.initialize(False, False, False, False, False, sbdPublicKey65.toBinStr(), False)
             with open(filePath, 'wb') as newWltFile:
+               myPKS.initialize(isStatic, useCompr, use160, isUser, isExt,
+                                sbdPubKey65.toBinStr(), chksumPres)
                newWltFile.write(myPKS.serialize())
             return myPKS
 
 
-   def savePMTAFile(self):
+   # Call for when we want to save a binary PMTA record to a file. The PMTA
+   # record will include a PKS record for the currently selected wallet. By
+   # default, all PKS flags will be false, except for the flag adding a checksum
+   # to the PKS record.
+   # INPUT:  PKS-related flags (bool) - See armoryengine/ConstructedScript.py
+   # OUTPUT: None
+   # RETURN: Final PMTA record (PMTARecord)
+   def savePMTAFile(self, isStatic = False, useCompr = False, use160 = False,
+                    isUser = False, isExt = False, chksumPres = True):
          defName = 'armory_%s.pmta' % self.wlt.uniqueIDB58
          payNet = PAYNET_BTC
          if getTestnetFlag():
             payNet = PAYNET_TBTC
 
          filePath = unicode(self.main.getFileSave(defaultFilename = defName))
-         if not len(filePath)>0:
-            print 'NOTHING SAVED!'
+         if not len(filePath) > 0:
             return
          else:
-            sbdPublicKey33 = SecureBinaryData(self.wlt.sbdPublicKey33)
-            sbdPublicKey65 = CryptoECDSA().UncompressPoint(sbdPublicKey33);
+            # Start with the wallet's uncompressed root key.
+            sbdPubKey33 = SecureBinaryData(self.wlt.sbdPubKey33)
+            sbdPubKey65 = CryptoECDSA().UncompressPoint(sbdPubKey33)
 
             pathdir = os.path.dirname(filePath)
-            pathfn  = os.path.basename(filePath)
-
             if not os.path.exists(pathdir):
-               raise FileExistsError('Path for new wlt does not exist: %s', pathdir)
+               raise FileExistsError('Path for new PKS record does not ' \
+                                     'exist: %s', pathdir)
 
-            if os.path.exists(filePath):
-               raise FileExistsError('File already exists, will not overwrite')
-
+            # Write the PMTA record to the file, then return the record.
+            myPMTA = PMTARecord()
             with open(filePath, 'wb') as newWltFile:
                myPKS = PublicKeySource()
-               myPKS.initialize(False, False, False, False, False, sbdPublicKey65.toBinStr(), True)
-               myPMTA = PMTARecord()
+               myPKS.initialize(isStatic, useCompr, use160, isUser, isExt,
+                                sbdPubKey65.toBinStr(), chksumPres)
                myPMTA.initialize(myPKS.serialize(), payNet)
                newWltFile.write(myPMTA.serialize())
+            return myPMTA
 
 
-   def setWallet(self, wlt, isDoubleClick=False):
+   # Callback function for when the user selects a wallet.
+   def setWallet(self, wlt):
       self.wlt = wlt
 
 
-   # Updates the wallet balance on startup.
-   def UpdateWallet(self):
+   # Function called when the "bdmReadyPMTA" signal is emitted. Updates the
+   # wallet balance on startup.
+   def bdmReady(self):
       self.frmSelectedWlt.updateOnWalletChange()
 
 
-   # Place any code here that must be executed once the BDM is ready. For now,
-   # the only thing we do is emit a signal so that the GUI can be updated by the
-   # main thread (Qt GUI requirement), which updates the wallet balance.
-   def handleNotification(self, action, args):
+   # Place any code here that must be executed when the BDM emits a signal. The
+   # only thing we do is emit a signal so that the call updating the GUI can be
+   # called by the main thread (Qt GUI requirement). This updates the wallet
+   # balance. If we attempt to update the GUI here (non-main thread), we crash.
+   def handleBDMNotification(self, action, args):
       if action == FINISH_LOAD_BLOCKCHAIN_ACTION:
-         self.main.emit(SIGNAL('pluginNotify'))
+         self.main.emit(SIGNAL('bdmReadyPMTA'))
 
 
-   #############################################################################
+   # Function is required by the plugin framework.
    def getTabToDisplay(self):
       return self.tabToDisplay
