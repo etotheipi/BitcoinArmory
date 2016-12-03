a7816cdb5327e45d570f4500a5511c655f1ea4dd
goatpig <moothecowlord@gmail.com>
2016-12-03 08:50:39 +0100

    add missing file

---
+++ armoryengine/CppSignerDirector.py
@@ -0,0 +1,26 @@
+################################################################################
+##                                                                            ##
+##  Copyright (C) 2016, goatpig                                               ##
+##  Distributed under the MIT license                                         ##
+##  See LICENSE-MIT or https://opensource.org/licenses/MIT                    ##
+##                                                                            ##
+################################################################################
+
+from armoryengine import PyBtcWallet
+import CppBlockUtils as Cpp
+
+class PythonSignerDirector(Cpp.PythonSigner):
+   def __init__(self, btcWallet):
+      Cpp.PythonSigner.__init__(self, btcWallet.cppWallet)
+      
+      self.wlt = btcWallet
+      
+   def getPrivateKeyForIndex(self, index):
+      return self.wlt.getPrivateKeyForIndex(index)
+   
+   def addSpender(self, utxo):
+      super(PythonSignerDirector, self).addSpender(\
+         utxo.val, \
+         0, 0, utxo.txOutIndex, \
+         utxo.txHash, utxo.binScript)
+      
