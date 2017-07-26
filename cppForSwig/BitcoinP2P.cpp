a3492e1d30ca72566e06285bda97b9d5bc539c1e
goatpig <moothecowlord@gmail.com>
2017-07-26 19:23:03 +0200

    bump version

--- cppForSwig/BitcoinP2P.cpp
+++ cppForSwig/BitcoinP2P.cpp
@@ -936,7 +936,7 @@ void BitcoinP2P::connectLoop(void)
          version.setVersionHeaderIPv4(70012, services, timestamp,
             node_addr_, clientsocketaddr);
 
-         version.userAgent_ = "Armory:0.96.0.4";
+         version.userAgent_ = "Armory:0.96.1";
          version.startHeight_ = -1;
 
          sendMessage(move(version));
