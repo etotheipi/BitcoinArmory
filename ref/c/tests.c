89c34683638088bcee73ff258b3e9b05ac84e0ae
Douglas Roark <joroark@vt.edu>
2018-11-11 14:36:46 -0800

    Squashed 'cppForSwig/bech32/' changes from bfc71671..bdc264f8
    bdc264f8 Merge pull request #45 from isghe/html-validation-w3
    498b6a7f html validation ok on https://validator.w3.org
    60848c2e Merge #43: Fix a buffer overflow in the C reference decoder
    2b0aac65 Fix a buffer overflow in the C reference decoder
    
    git-subtree-dir: cppForSwig/bech32
    git-subtree-split: bdc264f84014c234e908d72026b7b780122be11f

--- ref/c/tests.c
+++ ref/c/tests.c
@@ -91,6 +91,7 @@ static const char* invalid_address[] = {
     "BC13W508D6QEJXTDG4Y5R3ZARVARY0C5XW7KN40WF2",
     "bc1rw5uspcuh",
     "bc10w508d6qejxtdg4y5r3zarvary0c5xw7kw508d6qejxtdg4y5r3zarvary0c5xw7kw5rljs90",
+    "bca0w508d6qejxtdg4y5r3zarvary0c5xw7kw508d6qejxtdg4y5r3zarvary0c5xw7kw5rljs90234567789035",
     "BC1QR508D6QEJXTDG4Y5R3ZARVARYV98GJ9P",
     "tb1qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3q0sL5k7",
     "bc1zw508d6qejxtdg4y5r3zarvaryvqyzf3du",
